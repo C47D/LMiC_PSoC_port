c693c1617cdbd867109ff13834cf9b7e2fdfd1e2
C47D <carlos.santiago.diaz@gmail.com>
2017-06-01 00:45:22 -0500

    Fix warnings, fix typo hal_enable_IRQs to hal_enableIRQs and remove hal_spi_init, SPI is initialized in hal_init.

--- lmic_psoc_port/hal.c
+++ lmic_psoc_port/hal.c
@@ -1,30 +1,4 @@
 /*
- * All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions are met:
- *  * Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *  * Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *  * Neither the name of the <organization> nor the
- *    names of its contributors may be used to endorse or promote products
- *    derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-/*
 ** HAL targeted to PSoC5LP and SX1272 IC
 */
 
@@ -34,18 +8,11 @@
 #include "lmic.h"
 #include "debug.h"
 
-// HAL state
-static struct {
-    int irqlevel;
-    u4_t ticks;
-} HAL;
-
-// -----------------------------------------------------------------------------
-// I/O
-
 void hal_init( void )
 {
     //hal_time_init();
+    SPI_Start();
+    UART_Start();
     
     // Most of the HAL configuration is done on the schematic
     // so not so much to be done here.
@@ -131,14 +98,14 @@ void hal_disableIRQs( void )
     
 }
 
-void hal_enable_IRQs( void )
+void hal_enableIRQs( void )
 {
     
 }
 
 void hal_sleep( void )
 {
-    
+    // __WFI();
 }
 
 u4_t hal_ticks( void )
@@ -251,26 +218,6 @@ void EXTI15_10_IRQHandler () {
 #define GPIO_AF_SPI1        0x05
 
 #endif
-    
-static void hal_spi_init(void)
-{
-    // SPI Configuration is done on the schematic
-    SPI_Start();
-    
-#if 0
-    // enable clock for SPI interface 1
-    RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
-
-    // use alternate function SPI1 (SCK, MISO, MOSI)
-    hw_cfg_pin(GPIOx(SCK_PORT),  SCK_PIN,  GPIOCFG_MODE_ALT | GPIOCFG_OSPEED_40MHz | GPIO_AF_SPI1 | GPIOCFG_OTYPE_PUPD | GPIOCFG_PUPD_PDN);
-    hw_cfg_pin(GPIOx(MISO_PORT), MISO_PIN, GPIOCFG_MODE_ALT | GPIOCFG_OSPEED_40MHz | GPIO_AF_SPI1 | GPIOCFG_OTYPE_PUPD | GPIOCFG_PUPD_PDN);
-    hw_cfg_pin(GPIOx(MOSI_PORT), MOSI_PIN, GPIOCFG_MODE_ALT | GPIOCFG_OSPEED_40MHz | GPIO_AF_SPI1 | GPIOCFG_OTYPE_PUPD | GPIOCFG_PUPD_PDN);
-    
-    // configure and activate the SPI (master, internal slave select, software slave mgmt)
-    // (use default mode: 8-bit, 2-wire, no crc, MSBF, PCLK/2, CPOL0, CPHA0)
-    SPI1->CR1 = SPI_CR1_MSTR | SPI_CR1_SSI | SPI_CR1_SSM | SPI_CR1_SPE;
-#endif
-}
 
 #ifdef CFG_lmic_clib
 
